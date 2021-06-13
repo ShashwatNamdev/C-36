74b8a6317a0228037fee295ec8e0c9f40f2b87d0
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-06-13 10:58:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var hypnoticBall, database;
+var position;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+}
+
+function draw(){
+  background("white");
+  drawSprites();
+}
